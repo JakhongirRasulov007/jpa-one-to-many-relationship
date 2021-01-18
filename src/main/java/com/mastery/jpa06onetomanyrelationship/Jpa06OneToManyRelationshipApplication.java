bd35eadefcd54d58ce86549fedb757a7883c2a83
Jakhongir <jakhongirrasulov001@gmail.com>
2021-01-18 10:33:59 +0500

    one-to-many relationship in JPA

---
+++ src/main/java/com/mastery/jpa06onetomanyrelationship/Jpa06OneToManyRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.mastery.jpa06onetomanyrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa06OneToManyRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa06OneToManyRelationshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mastery/jpa06onetomanyrelationship/Jpa06OneToManyRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastery.jpa06onetomanyrelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa06OneToManyRelationshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
